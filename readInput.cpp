99e1e64113ad69b22a60b92b4078ce7f0aab38f5
Kristoffer Sandvang <kristoffersandvang@gmail.com>
2024-02-07 14:16:05 +0100

    Finished readInput
    Co-Authored-By: mfilitu <70143907+mfilitu@users.noreply.github.com>

--- readInput.cpp
+++ readInput.cpp
@@ -1,25 +1,25 @@
-/*Hell world program in C++*/
 #include <iostream>
 #include <fstream>
 #include <vector>
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    std::ifstream inputFile("test.gr");
+    std::ifstream inputFile("tests/test.gr");
     std::string line;
     std::vector<int> A, B;
     std::vector<std::pair<int, int> > edges;
     int n0, n1, m;
     A.clear();
     B.clear();
+    edges.clear();
+
     while (std::getline(inputFile, line))
     {
-        
         if (line[0] == 'c') {
             continue;
         }
         if (line.substr(0, 5) == "p ocr") {            
             std::sscanf(line.c_str(),"p ocr %d %d %d", &n0, &n1, &m);
-            // std::cout << n0 << " " << n1 << " " << m << std::endl;
+            
             for (int i = 1; i <= std::max(n0, n1); i++)
             {
                 if (i <= n0) {
@@ -29,24 +29,18 @@ int main() {
                     B.push_back(i + n0);
                 }
             }
-            //print vector A
-            std::cout << "A: " << std::endl;
-            for (int i = 0; i < A.size(); i++)
-            {
-                std::cout << A[i] << " ";
-            }
-            std::cout << "\nB: " <<std::endl;
-            for (int i = 0; i < B.size(); i++)
-            {
-                std::cout << B[i] << " ";
-            }
-            continue;
         }
-        
+
+        int edgeStart, edgeEnd;
+        if (std::sscanf(line.c_str(), "%d %d", &edgeStart, &edgeEnd) == 2)
+        {
+            std::pair<int, int> edge(edgeStart,edgeEnd);
+            edges.push_back(edge);
+        }
     }
-    // kode der laver de 3 arrays xD
+
     inputFile.close();
-    return 1;
+    return 0;
 }
 
 
