150e72d996a1759b7c1411106007bf65ec572251
mfilitu <70143907+mfilitu@users.noreply.github.com>
2024-04-30 17:53:01 +0200

    cmake

---
+++ build/CMakeFiles/solver.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/Barycenter.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/Barycenter.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/Barycenter.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/Crossingminimizer.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/Crossingminimizer.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/Crossingminimizer.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/Median.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/Median.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/Median.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/OptimizedBC.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/OptimizedBC.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/OptimizedBC.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/OptimizedBCRight.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/OptimizedBCRight.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/OptimizedBCRight.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/OptimizedMedian.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/OptimizedMedian.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/OptimizedMedian.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/CrossingMinimizers/ParentMinimizer.cpp" "CMakeFiles/solver.dir/CrossingMinimizers/ParentMinimizer.cpp.obj" "gcc" "CMakeFiles/solver.dir/CrossingMinimizers/ParentMinimizer.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/Graph.cpp" "CMakeFiles/solver.dir/Graph.cpp.obj" "gcc" "CMakeFiles/solver.dir/Graph.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/Solver.cpp" "CMakeFiles/solver.dir/Solver.cpp.obj" "gcc" "CMakeFiles/solver.dir/Solver.cpp.obj.d"
+  "C:/Users/mobtr/Desktop/PACE2024/Vertex.cpp" "CMakeFiles/solver.dir/Vertex.cpp.obj" "gcc" "CMakeFiles/solver.dir/Vertex.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
