5981ca7b499e93d4c5a117ae2c509cae3067efc7
Kristoffer Sandvang <kristoffersandvang@gmail.com>
2024-06-09 15:09:01 +0200

    bugfix
    Co-Authored-By: mfilitu <70143907+mfilitu@users.noreply.github.com>

--- Solver.sh
+++ Solver.sh
@@ -30,7 +30,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-g++ -std=c++17 -Ofast ./src/Hybrid.cpp ./src/Graph.cpp ./src/Vertex.cpp ./src/CrossingMinimizers/Crossingminimizer.cpp ./src/CrossingMinimizers/Barycenter.cpp ./src/CrossingMinimizers/Median.cpp ./src/CrossingMinimizers/OptimizedBC.cpp ./src/CrossingMinimizers/OptimizedMedian.cpp ./src/CrossingMinimizers/BarycenterMed.cpp ./src/CrossingMinimizers/BarycenterRev.cpp ./src/CrossingMinimizers/MedianBary.cpp ./src/CrossingMinimizers/MedianRev.cpp -o Hybrid.o
+g++ -std=c++17 -Ofast ./src/Hybrid.cpp ./src/Graph.cpp ./src/Vertex.cpp ./src/CrossingMinimizers/Crossingminimizer.cpp ./src/CrossingMinimizers/Barycenter.cpp ./src/CrossingMinimizers/Median.cpp ./src/CrossingMinimizers/OptimizedBC.cpp ./src/CrossingMinimizers/OptimizedMedian.cpp ./src/CrossingMinimizers/BarycenterMed.cpp ./src/CrossingMinimizers/MedianBary.cpp -o Hybrid.o
 
 if [ $? -ne 0 ]; then
   echo "Compilation failed"
